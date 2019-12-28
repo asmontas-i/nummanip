a60ec55f2c9de377856d87ac707d7de9efb2c111
Ignas Asmontas <ignas.asmontas@gmail.com>
2019-12-28 12:00:31 +0200

    Added variadinc function

--- calc/math.go
+++ calc/math.go
@@ -2,6 +2,11 @@ package calc
 
 
 // Add returs sum of two integers
-func Add(i int, j int) int {
-	return i + j
+func Add(numbers ...int) int {
+	sum := 0
+
+	for _, num := range numbers {
+		sum = sum + num
+	}
+	return sum
 }
